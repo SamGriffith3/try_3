f358fae02193cd67de4d8817f0463620d09a4f3e
SamGriffith3 <garfunkelnator@gmail.com>
2016-09-21 08:28:36 -0400

    Okay, what am I doing wrong still?

--- app/templates/views.py
+++ app/templates/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, Flask
 from app import app
-from .forms import SearchForm
+
 
 
 @app.route('/')
@@ -9,8 +9,19 @@ def index():
     return render_template('index.html',
                            title='Home')
 
+
 @app.route('/downloads')
 def downloads():
     return render_template('downloads.html',
                            title='Downloads')
 
+
+@app.route('/cat')
+def root():
+    return app.send_static_file('something.txt')
+
+
+@app.route('/dog')
+def dog():
+    return render_template('index.html',
+                           title='Dog')
